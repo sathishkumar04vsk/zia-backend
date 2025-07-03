eba95780640495156eec00603db946bcd70e79d5
sathishkumar04vsk <sathish04vsk@gmail.com>
2025-07-03 20:45:52 +0530

    cors updated

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const userRoutes = require("./routes/userRoutes");
 const foodRoutes = require("./routes/foodRoutes");
+const cros = require("cors");
 
 const connectDB = require("./config/db");
 const dontenv = require('dotenv');
@@ -10,6 +11,9 @@ dontenv.config();
 const app = express();
 connectDB();
 app.use(express.json());
+app.use(cros({
+  origin: process.env.FRONTEND_URL || "http://localhost:5000",
+}));
 
 app.use('/api/users/',userRoutes)
 app.use('/api/foods/',foodRoutes)
